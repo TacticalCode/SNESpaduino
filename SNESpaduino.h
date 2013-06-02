b19b87f36f437b620afff27f6727c0cd4a340a12
Damon Dransfeld <damon@tacticalcode.de>
2013-06-02 08:00:36 +0200

    Define constants for parsing state-values
    Using the BTN_ constants, you can easily parse the received value for the button states.
    Just AND-ing the state value with a constant results in a TRUE result,
    if the button for that specific constant was pushed.
    This way, you can easily check for single buttons to be pressed.
    `getButtons() & BTN_A` is true, if the A-Button was pushed.
    `!(~getButtons() & (BTN_A | BTN_B))` is true, if BOTH A and B were pushed.
    The next commit will provide an option to return non-inverted output.
    That's easier to check against multiple button presses.

--- SNESpaduino.h
+++ SNESpaduino.h
@@ -11,6 +11,19 @@
 #ifndef snesduino_h
 #define snesduino_h
 
+#define BTN_B 0x01
+#define BTN_Y 0x02
+#define BTN_SELECT 0x04
+#define BTN_START 0x08
+#define BTN_UP 0x10
+#define BTN_DOWN 0x20
+#define BTN_LEFT 0x40
+#define BTN_RIGHT 0x80
+#define BTN_A 0x100
+#define BTN_X 0x200
+#define BTN_L 0x400
+#define BTN_R 0x800
+
 #include "Arduino.h"
 
 class SNESpaduino
