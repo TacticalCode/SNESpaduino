a9989a797541e3a6eede392eff9ad64ba4e0113c
Damon Dransfeld <damon@tacticalcode.de>
2013-06-02 04:03:17 +0200

    Init core files with header
    Adding the core files, namely .cpp and .h project files, following the official Arduino LibraryTutorial: http://arduino.cc/en/Hacking/LibraryTutorial
    Also in the files: Meta containing my name, website, a very brief description as well as the used license, CC-BY-SA 3.0

---
+++ SNESpaduino.cpp
@@ -0,0 +1,9 @@
+/*******************************
+ *
+ *	File: SNESpaduino.cpp
+ *	Description: SNESpaduino library: Use an Arduino to read the button states of a SNES Gamepad
+ *	Author: Damon Dransfeld
+ *	Web: http://tacticalcode.de
+ *	License: CC-BY-SA 3.0 ( http://creativecommons.org/licenses/by-sa/3.0/legalcode )
+ *
+ ******************************/
---
+++ SNESpaduino.h
@@ -0,0 +1,9 @@
+/*******************************
+ *
+ *	File: SNESpaduino.h
+ *	Description: Header file for the SNESpaduino library
+ *	Author: Damon Dransfeld
+ *	Web: http://tacticalcode.de
+ *	License: CC-BY-SA 3.0 ( http://creativecommons.org/licenses/by-sa/3.0/legalcode )
+ *
+ ******************************/
