b7c42e107e0efc40412b8958094539ae408837c4
Damon Dransfeld <damon@tacticalcode.de>
2013-06-02 04:18:31 +0200

    Specify class members in SNESpaduino.h
    Specify which functions and variables will be used in the main library.
    Public members are:
    
    * SNESpaduino(int, int, int) - The constructor accepting 3 ints to specify the necessary pins
    
    * uint16_t getButtons(void) - The function which returns the button states
    
    Private members:
    
    * uint16_t state - A variable to store the buttons states that are being received
    
    * int i, PIN_LATCH, PIN_CLOCK, PIN_DATA - A counter and the pin numbers for latching, clocking and receiving data
    
    Includes Arduino.h and adds protection against accidental multiple includes.

--- SNESpaduino.h
+++ SNESpaduino.h
@@ -7,3 +7,20 @@
  *	License: CC-BY-SA 3.0 ( http://creativecommons.org/licenses/by-sa/3.0/legalcode )
  *
  ******************************/
+ 
+#ifndef snesduino_h
+#define snesduino_h
+
+#include "Arduino.h"
+
+class SNESpaduino
+{
+	public:
+		SNESpaduino(int latch, int clock, int data);
+		uint16_t getButtons(void);
+	private:
+		uint16_t state;
+		int i, PIN_LATCH, PIN_CLOCK, PIN_DATA;
+}
+
+#endif
