25272ec261d9c5bbaf0fd6f7b12ea1b8b528edfe
Damon Dransfeld <damon@tacticalcode.de>
2013-06-02 05:05:16 +0200

    Add the core logic to pull the button states from the SNES pad
    Adding the simple function to read from the pad's shift register.
    Here is how it works:
    
    * Send a pulse on the latch pin, the register stores the buttons' states in the register
    
    * Read the value on the data pin (and store it)
    
    * Send a pulse to the clock pin, the register shifts out the next bit of data
    
    * Repeat steps 2 and 3 until all 12 states have been pulled from the register
    
    To make further processing easier, received bits are returned inverted: When a button is pressed during the latch pulse, it sets the corresponding bit to 0 (logic low).
    Also, only 12 of the 16 bits in `uint16_t state` are used, so the 4 most significant bits will always be returned as 1, thus they must be ignored.
    A future commit will include constants to help parsing the returned data, some examples will also be added.

--- SNESpaduino.cpp
+++ SNESpaduino.cpp
@@ -7,3 +7,46 @@
  *	License: CC-BY-SA 3.0 ( http://creativecommons.org/licenses/by-sa/3.0/legalcode )
  *
  ******************************/
+
+#include "Arduino.h"
+#include "SNESpaduino.h"
+
+// Constructor: Init pins
+SNESpaduino::SNESpaduino(int latch, int clock, int data)
+{
+	// Store the latch, clock and data pin for later use
+	PIN_LATCH = latch;
+	PIN_CLOCK = clock;
+	PIN_DATA = data;
+
+	// Set correct modes for the communication pins
+	pinMode(PIN_LATCH, OUTPUT);
+	pinMode(PIN_CLOCK, OUTPUT);
+	pinMode(PIN_DATA, INPUT);
+
+}
+
+// Return the state of all buttons. 12 of the uint16_t's bits are used, the 4 MSBs must be ignored.
+uint16_t SNESpaduino::getButtons(void)
+{
+	// Init the button-state variable
+	state = 0;
+
+	// Latch the current buttons' state into the pad's register
+	digitalWrite(PIN_LATCH, HIGH);
+	digitalWrite(PIN_LATCH, LOW);
+
+	// Loop to receive 12 bits from the pad
+	for(i = 0; i < 12; i++)
+	{
+		// Read a button's state, shift it into the variable
+		state |= digitalRead(PIN_DATA) << i;
+
+		// Send a clock pulse to shift out the next bit
+		digitalWrite(PIN_CLOCK, HIGH);
+		digitalWrite(PIN_CLOCK, LOW);
+	}
+
+	// Return the inverted bits, as pressed buttons are represented by a logic low (0)
+	return ~state;
+}
